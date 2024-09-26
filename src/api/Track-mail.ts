8cc3c21af0e20bf12140b5fda25ed2fc675f9e18
Laksh-1512 <lakshpaul1512@gmail.com>
2024-09-26 11:50:40 +0530

    changed img file path

--- src/api/Track-mail.ts
+++ src/api/Track-mail.ts
@@ -2,20 +2,19 @@ import { Hono } from "hono";
 import {getConnInfo} from 'hono/bun'
 import Track from "../model/TrackmodelSchema";
 import { promises as fs } from "fs";
+const path = require('path');
 const app = new Hono();
 
 let imageBuffer:Buffer;
-(
-    async () => {
-        try{
-            imageBuffer=await fs.readFile(__dirname+"./assets/water-lily-3784022_640.jpg");
-        }
-        catch(err){
-            // console.log(123)
-            console.error(err);
+(async () => {
+    try {
+        // Use path.join to construct the file path correctly
+        const filePath = path.join(__dirname, 'assets', 'water-lily-3784022_640.jpg');
+        imageBuffer = await fs.readFile(filePath);
+    } catch (err) {
+        console.error(err); // Log the actual error
     }
-}
-)()
+})();
 
 app.get("/track-mail/:id",async (c) => {
     const id = c.req.param('id');
